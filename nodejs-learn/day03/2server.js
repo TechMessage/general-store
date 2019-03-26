346f7f507528cfb10426e777e392e873c8c2447b
TechMessage <lihqheu@outlook.com>
2019-03-26 16:50:35 +0800

    1

--- nodejs-learn/day03/2server.js
+++ nodejs-learn/day03/2server.js
@@ -13,14 +13,32 @@ http.createServer((req, res) => {
 
     let pathname = url.parse(req.url).pathname;
 
-    if(pathname == '/login') {
+    //请求的方法
+    let method = req.method.toLowerCase()//
 
+    /**
+     * get请求参数的获取
+     * url.parse(req.url, true).query // 返回的是对象
+     */
+
+
+    if(pathname == '/login' && method == 'get') {
+        //请求参数
+        console.log(url.parse(req.url, true).query)
         ejs.renderFile('views/login.ejs', {
             msg:'login页面'
         }, function (err, data) {
             res.end(data)
         })
-    }else if(pathname == '/dologin') {
+    }else if(pathname == '/dologin' && method == 'post') {
+        //post 请求参数 req 实际上是一个读取流
+        let body = '';
+        req.on('data', function (chunk) {
+            body += chunk;
+        })
+        req.on('end', function () {
+            console.log(body)
+        })
         ejs.renderFile('views/dologin.ejs', {
             msg:'登录',
             list:[1,2,3]
@@ -28,13 +46,6 @@ http.createServer((req, res) => {
             res.end(data)
         })
     }
-
-
-
-
-    
-
-   
 
 
 }).listen(8000)

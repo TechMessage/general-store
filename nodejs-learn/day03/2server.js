5572a26fcc97e24646ec30ab120b1095862bdafe
TechMessage <lihqheu@outlook.com>
2019-03-26 15:59:21 +0800

    1

--- nodejs-learn/day03/2server.js
+++ nodejs-learn/day03/2server.js
@@ -3,15 +3,38 @@ const http = require('http')
 const router = require('./model/router')
 const path = require('path')
 const ejs = require('ejs')
+const url = require('url')
 
 http.createServer((req, res) => {
 
 
     res.writeHead(200, {'content-type':'text/html;charset=utf-8'})
+
+
+    let pathname = url.parse(req.url).pathname;
+
+    if(pathname == '/login') {
+
+        ejs.renderFile('views/login.ejs', {
+            msg:'login页面'
+        }, function (err, data) {
+            res.end(data)
+        })
+    }else if(pathname == '/dologin') {
+        ejs.renderFile('views/dologin.ejs', {
+            msg:'登录',
+            list:[1,2,3]
+        }, function (err, data) {
+            res.end(data)
+        })
+    }
+
+
+
+
     
-    ejs.renderFile('static/test.ejs', {}, function (err, data) {
-        res.end(data)
-    })
+
+   
 
 
 }).listen(8000)

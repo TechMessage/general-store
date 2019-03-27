492e933d4c66d3511c3b9fffa19b56700b64bba5
TechMessage <lihqheu@outlook.com>
2019-03-27 13:22:42 +0800

    1

--- nodejs-learn/day05/03express.js
+++ nodejs-learn/day05/03express.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 
 const bodyParser = require('body-parser') //处理post请求的中间件
+const cookieParser = require('cookie-parser')
 
 let app = new express()
 
@@ -16,6 +17,10 @@ app.set('views', path.join(__dirname, 'views')) //模板位置目录
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+//cookie
+app.use(cookieParser('sign-id')); //cookie加密 sign-id 签名随机字符串
+
 
 app.use(function (req,res,next) {
     //匹配所有请求
@@ -51,6 +56,56 @@ app.get('/login', function (req, res) {
 app.post('/post', function (req, res) {
     // res.send('post提交数据')
     res.send(JSON.stringify(req.body))
+})
+
+app.get('/set', function (req,res) {
+    //设置cookie
+    res.cookie('username', 'czhsnga', {
+        maxAge:6000, //6s后失效
+        httpOnly:true, //客户端脚本不能访问
+        domain: '.aaa.com', //多个二级域名共享
+        // path:'/get', //仅仅 /get路由下可以访问到cookie
+        signed:true, //加密 cookie的value值会被加密
+    })
+
+    res.send('cookie设置完成')
+})
+
+app.get('/get', function (req, res) {
+    //使用获取cookie
+
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
+})
+
+
+//cookie的简单使用案例，记录用户的浏览信息
+app.get('/lvyou', function (req, res) {
+    //get传值获取
+    let city = req.query.city;
+    //获取cookies
+    let cities = req.signedCookies.cities;
+
+    if(cities) {
+        cities = JSON.parse(cities);
+        cities.push(city)
+    }else {
+        cities = [];
+        cities.push(city)
+    }
+
+    //设置cookie
+    res.cookie('cities',JSON.stringify(cities), {
+        maxAge:60000000, //6s后失效
+        httpOnly:true, //客户端脚本不能访问
+        domain: '.aaa.com', //多个二级域名共享
+        // path:'/get', //仅仅 /get路由下可以访问到cookie
+        signed:true, //加密 cookie的value值会被加密
+    })
+
+    // console.log(cities)
+
+    res.send(city)
 })
 
 app.listen(8000)

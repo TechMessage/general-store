83273882646d361ff2c362f8a6db98e0bd620fab
TechMessage <lihqheu@outlook.com>
2019-03-27 13:44:18 +0800

    1

--- nodejs-learn/day05/03express.js
+++ nodejs-learn/day05/03express.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 const bodyParser = require('body-parser') //处理post请求的中间件
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 let app = new express()
 
@@ -28,6 +29,26 @@ app.use(function (req,res,next) {
     next()
 })
 
+//session
+/**
+ * session是存在服务端的，可以配置redis和数据库实现持久化，
+ * session是在用户第一次请求时，在服务端生成并保存的，同时生成一个cookie，该cookie保存了session的id，
+ * cookie返回给客户端，第2次请求时cookie发送给服务端，通cookie中保存的session_id来找到对应的session信息
+ * 
+ * session是建立客户端和服务端数据保存的
+ * 
+ */
+app.use(session({
+    name:'session_id', //本地保存sessionid的cookie名
+    secret:'session-id',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        maxAge:30*1000*60 //30分钟cookie失效
+    },
+    rolling:true, //每次请求重置cookie的失效时间，每次
+}))
+
 
 //配置静态文件web服务
 app.use(express.static(path.join(__dirname, 'public'))) 
@@ -48,9 +69,18 @@ app.get('/product', function (req, res) {
 })
 
 app.get('/login', function (req, res) {
+    //保存session
+    req.session.userinfo = 'zhangs';
+
     res.render('login', {
         msg:'hello ejs xxx'
     })
+})
+
+app.get('/user', function (req,res) {
+    //获取session信息
+    console.log(req.session.userinfo)
+    res.send(req.session.userinfo)
 })
 
 app.post('/post', function (req, res) {
@@ -96,7 +126,7 @@ app.get('/lvyou', function (req, res) {
 
     //设置cookie
     res.cookie('cities',JSON.stringify(cities), {
-        maxAge:60000000, //6s后失效
+        maxAge:60*1000*10, //10分钟
         httpOnly:true, //客户端脚本不能访问
         domain: '.aaa.com', //多个二级域名共享
         // path:'/get', //仅仅 /get路由下可以访问到cookie

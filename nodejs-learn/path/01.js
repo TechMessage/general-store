7e2e4e99e996421432a29091b97c7c6144c6c812
TechMessage <lihqheu@outlook.com>
2019-05-08 10:39:39 +0800

    1

---
+++ nodejs-learn/path/01.js
@@ -0,0 +1,20 @@
+
+
+const path = require('path')
+
+console.log(path.delimiter) //分隔符
+
+// console.log(process.env)
+
+console.log(path.extname('./adfads/asdfas/teet.html'))//文件扩展名
+
+
+console.log(path.join(__dirname, '/fa','h.html'))
+
+
+console.log(path.parse('./fasd/dfasd/1.dat'))
+
+
+
+
+

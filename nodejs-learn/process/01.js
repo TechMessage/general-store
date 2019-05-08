8d4173921cc541b5053a9f9c47f8596822d73240
TechMessage <lihqheu@outlook.com>
2019-05-08 16:30:11 +0800

    1

---
+++ nodejs-learn/process/01.js
@@ -0,0 +1,27 @@
+
+/**
+ * process 对象是全局变量，表示当前nodejs进程的
+ * 相关信息， 同时它是EventEmitter的实例对象
+ */
+
+
+
+// process.on('beforeExit', function (code) {
+//     console.log('process will being exit', code)
+// })
+
+// process.on('exit', function (code) {
+//     console.log('process exited', code)
+// })
+
+
+// console.log(process.arch)  //x64
+
+// console.log(process.cpuUsage())
+
+
+
+
+
+
+
